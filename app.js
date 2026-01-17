043a14e2cdb969cbbb4e7cbb0cfcff232675e107
Aditya <bansaladitya523@gmail.com>
2026-01-17 21:31:33 +0530

    new updates

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let inp = document.querySelector(".container input");
 let ul = document.querySelector(".container ul");
 let addBtn = document.querySelector(".container #addBtn");
+let log = document.querySelector(".login");
+let about = document.querySelector(".about");
+let loginClose = document.querySelector(".login h2 i");
+let signClose = document.querySelector(".sign h2 i");
+let aboutClose = document.querySelector(".about h2 i");
+let sig = document.querySelector(".sign");
+let todo_container = document.querySelector(".todo_container");
 
 let taskAll = getTodoFromLS();
 
@@ -10,16 +17,34 @@ function createTodo(task, idx) {
   if (task) {
     let li = document.createElement("li");
     let del = document.createElement("button");
-    li.innerText = `${task}`;
+    li.innerHTML = `<i class="fa-regular fa-square-check"></i>${task.Todo}`;
+
     del.innerText = "Delete";
     del.addEventListener("click", () => {
       delTodoFromLS(idx);
     });
+
+    let i = li.querySelector("i");
+    i.addEventListener("click", () => {
+      isDone(i, idx);
+    });
+    if (task.isDone == false) {
+      i.classList.add("fa-regular");
+      i.classList.remove("fa-solid");
+    } else {
+      li.style.opacity = "0.4";
+      i.classList.remove("fa-regular");
+      i.classList.add("fa-solid");
+    }
     li.append(del);
     ul.append(li);
   }
 }
 
+function isDone(i, idx) {
+  taskAll[idx].isDone = taskAll[idx].isDone ? false : true;
+  saveToLS();
+}
 function updateTodoList() {
   if (taskAll) {
     taskAll.forEach((task, idx) => {
@@ -41,7 +66,7 @@ addBtn.addEventListener("click", () => {
 function accessInputValue() {
   let todo = inp.value;
   if (todo) {
-    taskAll.push(todo);
+    taskAll.push({ Todo: todo, isDone: false });
     inp.value = "";
     saveToLS();
   } else {
@@ -64,4 +89,32 @@ function getTodoFromLS() {
   let todoList = JSON.parse(localStorage.getItem("data"));
   return todoList || [];
 }
-console.log("hello");
+
+function login() {
+  log.classList.remove("displayClose");
+  log.classList.add("displaay");
+  sig.classList.add("displayClose");
+  about.classList.add("displayClose");
+}
+function sign() {
+  log.classList.add("displayClose");
+  sig.classList.remove("displayClose");
+  sig.classList.add("displaay");
+  about.classList.add("displayClose");
+}
+function aboutUs() {
+  log.classList.add("displayClose");
+  about.classList.remove("displayClose");
+  about.classList.add("displaay");
+  sig.classList.add("displayClose");
+}
+
+loginClose.addEventListener("click", function () {
+  log.classList.toggle("displayClose");
+});
+signClose.addEventListener("click", function () {
+  sig.classList.toggle("displayClose");
+});
+aboutClose.addEventListener("click", function () {
+  about.classList.toggle("displayClose");
+});
